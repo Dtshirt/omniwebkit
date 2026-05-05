d63974828eba5066dde153976d9157799ce7afd5
Deepak <deepakbaradwaj933@gmail.com>
2026-05-06 00:13:40 +0530

    a

--- components/tools/HomePageSections.js
+++ components/tools/HomePageSections.js
@@ -1,5 +1,3 @@
-'use client';
-
 import Link from 'next/link';
 import { ArrowRight, TrendingUp, Star } from 'lucide-react';
 import { categories, getPopularTools } from '@/lib/tools-data';
--- components/tools/ToolCard.js
+++ components/tools/ToolCard.js
@@ -1,5 +1,3 @@
-'use client';
-
 import Link from 'next/link';
 import { Star, ArrowRight, Zap } from 'lucide-react';
 
